347e977d0b96c534d88f938e0151c9dabe75729c
Wesley Monteiro de Araujo <90140946+WesleyMAraujo@users.noreply.github.com>
2026-02-26 09:07:25 -0300

    Usa persistência nativa do Filament no criar outro de dívidas

--- app/Filament/Resources/DividaResource/Pages/CreateDivida.php
+++ app/Filament/Resources/DividaResource/Pages/CreateDivida.php
@@ -3,10 +3,18 @@
 namespace App\Filament\Resources\DividaResource\Pages;
 
 use App\Filament\Resources\DividaResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateDivida extends CreateRecord
 {
     protected static string $resource = DividaResource::class;
+
+    protected function preserveFormDataWhenCreatingAnother(): array
+    {
+        return [
+            'data_inicio',
+            'cartao_id',
+            'pessoa_id',
+        ];
+    }
 }
